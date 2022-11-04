cf27150a4fcf0f24e25eba52fc2eb51c6b43d876
MarynaAponasko <aponasko.mari@gmail.com>
2022-11-04 19:40:18 +0200

    Task 2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (const obj of ingredients) {
+  const newItem = document.createElement("li");
+  newItem.textContent = obj;
+  newItem.classList.add("item");
+  list.append(newItem);
+}
