5ab547e3ec121e2d0e3bd6abb6bccefd65836655
MarynaAponasko <aponasko.mari@gmail.com>
2022-11-05 18:47:00 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const body = document.querySelector("body");
 const colorBtn = document.querySelector(".change-color");
 const span = document.querySelector(".color");
